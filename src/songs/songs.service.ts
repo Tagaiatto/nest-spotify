c62288e5511bbaef632b50378b290bcb0c19f243
Edilson Mendes <edilsonmneto@gmail.com>
2024-09-17 20:32:51 -0300

    added song entity to songs module and songs service

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Song } from './entities/songs.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SongsService {
     private readonly songs = [];
+
+    constructor(
+        @InjectRepository(Song)
+        private songRepository: Repository<Song>
+    ) {}
 
     create(song){
         this.songs.push(song);
