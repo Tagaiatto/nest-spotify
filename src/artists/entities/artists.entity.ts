38ca6e44964f771fbb6d25f26ce97c3cd1f287da
Tagaiatto <edilsonmneto@gmail.com>
2024-09-24 19:08:40 -0300

    added artists entity

---
+++ src/artists/entities/artists.entity.ts
@@ -0,0 +1,16 @@
+import {
+    User
+} from 'src/users/entities/users.entity';
+
+import {
+    Entity,
+    JoinColumn,
+    OneToOne,
+    PrimaryGeneratedColumn
+} from 'typeorm';
+
+@Entity('artists')
+export class Artist {
+    @PrimaryGeneratedColumn()
+    id: number;
+}
