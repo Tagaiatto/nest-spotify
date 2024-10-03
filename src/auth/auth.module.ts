6603cde921e54182f92a65daf87da083ff47bc3b
Tagaiatto <edilsonmneto@gmail.com>
2024-10-03 16:18:15 -0300

    added jwtmodule register to import on auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [UsersModule],
+    imports: [UsersModule, JwtModule.register({ secret: 'HAD_12X#@' })],
     controllers: [AuthController],
     providers: [AuthService],
     exports: [AuthService]
