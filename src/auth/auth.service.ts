834009bb9060c8a876aaf14715f4011c242f3e6a
Tagaiatto <edilsonmneto@gmail.com>
2024-10-03 16:58:09 -0300

    added jwt return to login method in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,16 @@ import { LoginDTO } from './dto/login-dto';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcryptjs';
 import { User } from 'src/users/entities/users.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(
+        private readonly usersService: UsersService,
+        private jwtService: JwtService
+    ) {}
 
-    async login(loginDTO: LoginDTO): Promise<User> {
+    async login(loginDTO: LoginDTO): Promise<{ accessToken: string }> {
         const user = await this.usersService.findOne(loginDTO);
         const passwordMatched = await bcrypt.compare(
             loginDTO.password,
@@ -16,7 +20,11 @@ export class AuthService {
         );
         if(passwordMatched){
             delete user.password;
-            return user;
+            // return user;
+            const payload = { email: user.email, sub: user.id };
+            return {
+                accessToken: this.jwtService.sign(payload)
+            };
         } else {
             throw new UnauthorizedException('Invalid username or password');
         }
