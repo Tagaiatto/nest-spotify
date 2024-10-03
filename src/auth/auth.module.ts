03a27cf514f67db34b8d05861e98940c75020119
Tagaiatto <edilsonmneto@gmail.com>
2024-10-03 18:15:55 -0300

    added auth constants and refactor auth module with it

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,13 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { authConstants } from './auth.constants';
 
 @Module({
-    imports: [UsersModule, JwtModule.register({ secret: 'HAD_12X#@' })],
+    imports: [UsersModule, JwtModule.register({ 
+        secret: authConstants.secret,
+        signOptions: { expiresIn: '1d' }
+    })],
     controllers: [AuthController],
     providers: [AuthService],
     exports: [AuthService]
