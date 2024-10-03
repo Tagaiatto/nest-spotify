14f877af694563dc56e82dce711237734c26c50c
Tagaiatto <edilsonmneto@gmail.com>
2024-10-03 12:08:03 -0300

    added login method to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDTO } from './dto/login-dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs';
+import { User } from 'src/users/entities/users.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly usersService: UsersService) {}
+
+    async login(loginDTO: LoginDTO): Promise<User> {
+        const user = await this.usersService.findOne(loginDTO);
+        const passwordMatched = await bcrypt.compare(
+            loginDTO.password,
+            user.password
+        );
+        if(passwordMatched){
+            delete user.password;
+            return user;
+        } else {
+            throw new UnauthorizedException('Invalid username or password');
+        }
+    }
+}
